8c8aac30735daa68a7366eb2aa33046d2dec12aa
Raymond Chow <raychow1944@yahoo.com>
2018-12-22 01:07:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,4 +6,10 @@ def key_for_min_value(name_hash)
   name_hash.collect do |key, value|
     values << value 
   end
+  min_value = values[0]
+  values.each do |value|
+    if value < min_value
+      min_value = value
+    end
+  end
 end
